d14d9eab7ca0a7f0c6314d0fae67a4bab5cfb4f3
mgladchuk <mhladchuk@gmail.com>
2018-02-27 21:16:00 +0200

    first issue, first solution

--- Interview.WrapText/src/interview/WrapText.java
+++ Interview.WrapText/src/interview/WrapText.java
@@ -2,21 +2,46 @@ package interview;
 
 /**
  * Implement a function that takes an arbitrary natural language text and wraps it, such that:
- * 
+ * <p>
  * <ol>
- *   <li>Every line is indented with four spaces</li>
- *   <li>No line is longer than 80 characters in length</li>
- *   <li>Words are not cut in the middle</li>
+ * <li>Every line is indented with four spaces</li>
+ * <li>No line is longer than 80 characters in length</li>
+ * <li>Words are not cut in the middle</li>
  * </ol>
  */
 
-public final class WrapText
-{
-    public static String wrap( final String text )
-    {
-        // TODO
-        
-        return null;
+public final class WrapText {
+
+    static final String FOUR_SPACES = "    ";
+    private static final int MAX_LENGHT = 76;
+
+    private WrapText() {
+
     }
-    
+
+    public static String wrap(final String text) {
+        String[] words = text.split("\\s+");
+        StringBuilder resultBuilder = new StringBuilder();
+        int counter = MAX_LENGHT;
+        for (String word : words) {
+            if (word.length() <= counter) {
+                counter = getCounter(resultBuilder, counter, word);
+            } else {
+                resultBuilder.append(FOUR_SPACES);
+                resultBuilder.append(System.lineSeparator());
+                counter = getCounter(resultBuilder, MAX_LENGHT, word);
+            }
+        }
+
+        return resultBuilder.toString();
+    }
+
+    private static int getCounter(StringBuilder resultBuilder, int counter, String word) {
+        int val = counter;
+        resultBuilder.append(word);
+        resultBuilder.append(" ");
+        val -= word.length() + 1;
+        return val;
+    }
+
 }
